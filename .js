cd2e2464757eb83cb50884600180b73c378d63d8
Adithyanarayanan <101406636+adithyan-sajan@users.noreply.github.com>
2025-03-07 10:47:45 +0530

    Complete initial setup of game
    Make all necessary function of the game and prototyping of the main logic.

---
+++ .js
@@ -0,0 +1,64 @@
+
+let humanScore = 0, computerScore = 0;
+
+function getComputerChoice() {
+    let num = Math.floor(Math.random() * 3);
+    switch (num) {
+        case 0:
+            return "rock";
+        case 1:
+            return "paper";
+        case 2:
+            return "scissor";
+    }
+}
+function getHumanChoice() {
+    choice = prompt("Enter your choice").toLowerCase();
+    return choice
+}
+
+function playRound(humanChoice, computerChoice) {
+    switch (humanChoice) {
+        case "rock":
+            if (computerChoice == "scissor") {
+                humanScore++;
+                break;
+            }
+            else if (computerChoice == "paper") {
+                computerScore++;
+                break;
+            }
+            else
+                break;
+        case "paper":
+            if (computerChoice == "rock") {
+                humanScore++;
+                break;
+            }
+            else if (computerChoice == "scissor") {
+                computerScore++;
+                break;
+            }
+            else
+                break;
+        case "scissor":
+            if (computerChoice == "paper") {
+                humanScore++;
+                break;
+            }
+            else if (computerChoice == "rock") {
+                computerScore++;
+                break;
+            }
+            else
+                break;
+    }
+    console.log(humanChoice);
+    console.log(computerChoice);
+    console.log(humanScore, computerScore);
+}
+for (let i = 0; i < 5; i++) {
+    var computerChoice = getComputerChoice();
+    var humanChoice = getHumanChoice();
+    playRound(humanChoice, computerChoice);
+}
